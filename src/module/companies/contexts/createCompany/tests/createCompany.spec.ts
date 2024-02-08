d52223610d75b1bb096e587d058c2f6b8dd2b57d
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2024-02-08 12:46:44 -0300

    test: create validation for erro scenarios

--- src/module/companies/contexts/createCompany/tests/createCompany.spec.ts
+++ src/module/companies/contexts/createCompany/tests/createCompany.spec.ts
@@ -6,6 +6,9 @@ import { CreateCompanyController } from '../createCompany.controller';
 import { CompanyRepository } from '@shared/repositories/company.repository';
 import { requestMockDTO } from './mocks/request.mock';
 import { repositoryMockResponse } from './mocks/response.mock';
+import { HttpStatus } from '@nestjs/common';
+import { BussinesException } from '@shared/exceptions/BusinessException';
+import { AlreadExistsException } from '@shared/exceptions/AlreadyExistException';
 
 describe('Create company', () => {
   let controller: CreateCompanyController;
@@ -55,5 +58,38 @@ describe('Create company', () => {
     });
   });
 
-  //it('Should be fail when cnpj is invalid')
+  it('Should be fail when cnpj is invalid', async () => {
+    vi.spyOn(repository, 'findByCNPJ').mockResolvedValue(null);
+    vi.spyOn(repository, 'create').mockResolvedValue(null);
+    try {
+      await controller.handle({ ...requestMockDTO, cnpj: '36212983000178' });
+    } catch (error: BussinesException | any) {
+      expect(repository.findByCNPJ).toBeCalledTimes(0);
+      expect(repository.create).toBeCalledTimes(0);
+
+      expect(error).toBeInstanceOf(BussinesException);
+      expect(error.status).toBe(HttpStatus.BAD_REQUEST);
+      expect(error.response.title).toBe('Violation of business rules');
+      expect(error.response.message).toBe('Invalid CNPJ');
+    }
+  });
+
+  it('Should be fail when cnpj is already exists in database', async () => {
+    vi.spyOn(repository, 'findByCNPJ').mockResolvedValue(
+      repositoryMockResponse,
+    );
+    vi.spyOn(repository, 'create').mockResolvedValue(null);
+    try {
+      await controller.handle(requestMockDTO);
+    } catch (error: AlreadExistsException | any) {
+      expect(repository.findByCNPJ).toBeCalledTimes(1);
+      expect(repository.findByCNPJ).toBeCalledWith(requestMockDTO.cnpj);
+      expect(repository.create).toBeCalledTimes(0);
+
+      expect(error).toBeInstanceOf(AlreadExistsException);
+      expect(error.status).toBe(HttpStatus.CONFLICT);
+      expect(error.response.title).toBe('Conflict');
+      expect(error.response.message).toBe('Company already exists');
+    }
+  });
 });
