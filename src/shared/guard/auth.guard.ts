73fc8f888dbda1d9fcfde191d2c15a9951793c99
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2024-02-08 23:04:51 -0300

    feat: change exception

--- src/shared/guard/auth.guard.ts
+++ src/shared/guard/auth.guard.ts
@@ -1,10 +1,6 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@shared/exceptions/UnauthorizedException';
 import { Request } from 'express';
 
 @Injectable()
@@ -15,7 +11,7 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Missing access token');
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -24,7 +20,7 @@ export class AuthGuard implements CanActivate {
 
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid access token');
     }
     return true;
   }
