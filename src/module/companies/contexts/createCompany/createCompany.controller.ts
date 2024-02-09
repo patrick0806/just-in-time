d8d444ab249641e2139b72e60be908d91f944c8b
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2024-02-08 23:05:31 -0300

    feat: protect company routes

--- src/module/companies/contexts/createCompany/createCompany.controller.ts
+++ src/module/companies/contexts/createCompany/createCompany.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { API_TAGS } from '@shared/constants/apiTags';
 import { CreateCompanyRequestDTO } from './dtos/request.dto';
 import { CreateCompanyResponseDTO } from './dtos/response.dto';
 import { Problem } from '@shared/filters/Problem';
 import { CreateCompanyService } from './createCompany.service';
+import { AuthGuard } from '@shared/guard/auth.guard';
 
 @ApiTags(API_TAGS.COMPANY)
 @Controller()
+@UseGuards(AuthGuard)
 export class CreateCompanyController {
   constructor(private createCompanyService: CreateCompanyService) {}
 
   @ApiOperation({ summary: 'Create a new company' })
+  @ApiBearerAuth()
   @ApiBody({ type: CreateCompanyRequestDTO })
   @ApiResponse({ status: HttpStatus.CREATED, type: CreateCompanyResponseDTO })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, type: Problem })
--- src/module/companies/contexts/findCompanyByID/findCompanyById.controller.ts
+++ src/module/companies/contexts/findCompanyByID/findCompanyById.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { API_TAGS } from '@shared/constants/apiTags';
 import { FindCompanyByIdResponseDTO } from './dtos/response.dto';
 import { Problem } from '@shared/filters/Problem';
 import { FindCompanyByIdService } from './findCompanyById.service';
+import { AuthGuard } from '@shared/guard/auth.guard';
 
 @ApiTags(API_TAGS.COMPANY)
 @Controller()
+@UseGuards(AuthGuard)
 export class FindCompanyByIdController {
   constructor(private findCompanyByIdService: FindCompanyByIdService) {}
 
   @ApiOperation({ summary: 'Find Company by Id' })
+  @ApiBearerAuth()
   @ApiResponse({ status: HttpStatus.OK, type: FindCompanyByIdResponseDTO })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, type: Problem })
   @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR, type: Problem })
