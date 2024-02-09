8c7904915b10b2ff9674a28d393c34a1e3a04e21
Patrick da Silva Nicezi <patrickk0806@gmail.com>
2024-02-09 08:47:38 -0300

    feat: verify in auth guard if is public route

--- src/shared/guard/auth.guard.ts
+++ src/shared/guard/auth.guard.ts
@@ -1,13 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from '@shared/decorators/public.decorator';
 import { UnauthorizedException } from '@shared/exceptions/UnauthorizedException';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
